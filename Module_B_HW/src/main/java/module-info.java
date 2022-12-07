1bd5c0d077e84f6dc7f43c6dd4b6cd97b7b4e05a
SplashingYou <117105654+SplashingYou@users.noreply.github.com>
2022-12-07 13:09:56 -0500

    Delete Module_B_HW directory

--- Module_B_HW/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.module_b_hw {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.module_b_hw to javafx.fxml;
-    exports com.mycompany.module_b_hw;
-    
-}
